084f34f0a303ec4383aabfb0ad7c90876d28000d
D4B2A <128427718+D4B2A@users.noreply.github.com>
2024-02-14 14:46:03 +0100

    Create Wiring.h
    Added Pin Connections

---
+++ src/arduino/uno/Wiring.h
@@ -0,0 +1,9 @@
+//This file contains all definitions for the wiring
+
+#define ENABLEPIN 2
+#define PINA 3
+#define PINB 4
+#define POTIPIN A0
+#define CLOCKWISEPIN 5
+#define COUNTERCLOCKWISEPIN 6
+
