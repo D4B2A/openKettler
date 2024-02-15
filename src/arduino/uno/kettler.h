dc37a0b4601fd60b011a502f2697ee84d342bd65
D4B2A <128427718+D4B2A@users.noreply.github.com>
2024-02-15 21:43:04 +0100

    Created kettler.h

--- src/arduino/uno/kettler.h
+++ src/arduino/uno/kettler.h
@@ -1,1 +1,18 @@
+#define TRIGGERTOLERANCE 20
+#define TRIGGERDELAY 200
+#define CYCLEDELAY 5
+#define STOPTOLERANCE 10
 
+#define LOWLIMIT 50;
+#define HIGHLIMIT 500;
+
+enum motorState {
+    FORWARDS,
+    FREERUNNING,
+    STOP,
+    BACKWARDS
+};
+
+void updateMotor() {}
+void initMotor() {}
+int setTargetPos(int newTargetPos) {}
